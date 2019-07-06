c9f28c0e7a07ffd6e7f16fdf1300b41f5a532b68
Acis <akislatuconsina@gmail.com>
2019-07-06 16:01:59 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'testing',
+    component: HeaderComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
